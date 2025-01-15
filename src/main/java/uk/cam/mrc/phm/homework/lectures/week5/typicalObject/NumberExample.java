409fc2dcd1386670c023678eb0c8d39754aab60a
Qinnnnn <cindy.qinzhang@gmail.com>
2025-01-15 15:07:03 +0000

    upload homeworks including 1. lecture exercise and solutions 2. assignment 1,2,3 solutions

--- src/main/java/uk/cam/mrc/phm/homework/lectures/week5/typicalObject/NumberExample.java
+++ src/main/java/uk/cam/mrc/phm/homework/lectures/week5/typicalObject/NumberExample.java
@@ -2,11 +2,20 @@ package uk.cam.mrc.phm.homework.lectures.week5.typicalObject;
 
 public class NumberExample {
     public static void main(String[] args) {
-        Integer i1 = new Integer(1);
+
+        // Deprecated way since version 9 (should avoid this):
+        // Integer i1 = new Integer(1);
+
+        // Recommended way (use this instead):
+        Integer i1 = Integer.valueOf(1);
         Integer i2 = 1;
         System.out.println(i1.equals(i2));
 
-        Double d1 = new Double(0.2);
+        // Deprecated way since version 9 (should avoid this):
+        // Double d1 = new Double(0.2);
+
+        // Recommended way (use this instead):
+        Double d1 = Double.valueOf(0.2);
         Double d2 = 0.2;
         System.out.println(d1.equals(d2));
 
